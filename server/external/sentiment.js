05d40d469960a13e7e4b86bc79967ee215f0e84e
zhen <xxgiamxx@gmail.com>
2015-05-13 11:36:48 -0700

    Add sentiment

---
+++ server/external/sentiment.js
@@ -0,0 +1,26 @@
+var indico = require('indico.io');
+indico.apiKey = "da921676219f8f3e9aee66f98ae9c948";
+
+
+
+module.exports = {
+	getSentiment: function(words, cb)
+	{
+		indico.sentiment(words)
+	  .then(function(res) {
+	  	cb(res);
+	  }).catch(function(err) {
+	    console.warn(err);
+	  });
+	},
+
+	batchGetSentiment: function(listOfWords, cb)
+	{
+		indico.batchSentiment(listOfWords)
+	  .then(function(res) {
+	    cb(res);
+	  }).catch(function(err) {
+	    console.warn(err);
+	  });
+	}
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose')
 
+
 var db_port = process.env.MONGOLAB_URI || 'mongodb://localhost/socialstocks';
 
 mongoose.connect(db_port);
